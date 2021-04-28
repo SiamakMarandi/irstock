1504568af10f458ecf1d81c3046adc44137030c9
yoda <Siamak>
2021-04-28 09:30:25 +0430

    commit after adding captcha

--- irstocks/bourse/forms.py
+++ irstocks/bourse/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from captcha.fields import CaptchaField
 from django.core.exceptions import ValidationError
 # from django.contrib.auth.models import User
 from .models import User
@@ -10,7 +11,10 @@ class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
     last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
     email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+    captcha = CaptchaField()
 
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'email', 'password1', 'password2', )
+
+
--- irstocks/irstocks/settings.py
+++ irstocks/irstocks/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'jquery',
     'bourse',
+    'captcha',
 ]
 
 MIDDLEWARE = [
